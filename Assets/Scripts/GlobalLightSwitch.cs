0e22ab1e22a73b79e97e27706b61d9dd2d4f8bdc
Jason40004 <jcourtois@comcast.net>
2023-04-18 11:33:21 -0400

    Latest changes With dialogue
    Commit 1
    	• Added my temp dialogue API
    	• Added toggle for player light - this can be separate from the other global light states cause its always going to be inverse with global light on or off
    	• Added conditions for interacting or moving in player attributes - If player is reading dialogue, they can't do anything
    Commit 2
    	• Added enum state to player attributes
    	• Added tags to displaybox, text and player light, and flashlight
    		○ Now in global lightswitch code it finds the objects directly
    Dialogue API also finds text box and text in box objects directly

--- Assets/Scripts/GlobalLightSwitch.cs
+++ Assets/Scripts/GlobalLightSwitch.cs
@@ -7,16 +7,20 @@ using UnityEngine.Rendering.Universal;
 public class GlobalLightSwitch : MonoBehaviour, IInteractable
 {
     private Light2D globalLight;
+
+    private Light2D playerLight;
     // Start is called before the first frame update
     void Start()
     {
         globalLight = GameObject.FindGameObjectWithTag("GlobalLight").GetComponent<Light2D>();
+        playerLight = GameObject.FindGameObjectWithTag("PlayerLight").GetComponent<Light2D>();
     }
 
     // Toggles global lightswitch
     public void Interact()
     {
         globalLight.enabled = !globalLight.enabled;
+        playerLight.enabled = !playerLight.enabled;
     }
 
     // Update is called once per frame
